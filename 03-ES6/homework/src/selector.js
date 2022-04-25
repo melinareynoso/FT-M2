bbe2c85cf020b9c5a850fb0902be904a0c361acf
Melina <melina.reynoso@hotmail.com>
2022-04-25 01:09:17 -0300

    HW ES6 falta Extra

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl);
+
+  for (let i = 0; i < startEl.children.length; i++){
+    let child = startEl.children[i];
+    let elementsFound = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...elementsFound];
+  }
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +26,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === "#") return "id";
+  if(selector[0] === ".") return "class";
+  if(selector.split(".").length === 2) return "tag.class";
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +41,38 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = function(element){
+     //element (element HTML) tiene el id que estoy buscando?
+     return "#" + element.id === selector; // idOne === #idOne
+   }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(element){
+      //element (element HTML) tiene la clase que estoy buscando?
+      let classes = element.classList;
+      for (let i = 0; i < classes.length; i++) {
+        if("." + classes[i] === selector){
+          return true
+        }
+      }
+      return false
+    }
   } else if (selectorType === "tag.class") {
+    matchFunction = function(element){
+      //element (element HTML) tiene el tag que estoy buscando Y la clase que estoy buscando?
+      let[t,c] = selector.split("."); // ["div", "classOne"]
+
+      // PASO 1: let funcionTag = matchFunctionMaker(t);
+      // PASO 2: functionTag(element);
+      // LO HAGO EN UN SOLO PASO ↓
+      return matchFunctionMaker(t)(element) && matchFunctionMaker("." + c)(element);
+    }
     
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(element){
+      //element (element HTML) tiene el tag que estoy buscando?
+      return element.tagName === selector.toUpperCase();
+      // return element.localName === selector 
+    }
   }
   return matchFunction;
 };
